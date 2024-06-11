7a3e7796d07a049ae72da758d3d84e7d9d317dbd
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2024-06-12 00:11:56 +0200

    fix css

--- app/Components/Design/Hero.tsx
+++ app/Components/Design/Hero.tsx
@@ -11,17 +11,17 @@ const Hero = ({ lang }: any) => {
         <div className="flex w:11/12 lg:w-9/12 m-auto justify-center lg:justify-start w-full lg:h-2/3 xl2:pt-20 md:pt-6 pt-16">
           <div className="text-white w-10/12 lg:w-7/12 m-auto lg:ml-0 text-center lg:text-left relative text-redesignWhite">
             <h1
-              className={`text-4xl lg:text-8xl font-bold uppercase ${fontK2dBold.className}`}
+              className={`text-5xl lg:text-8xl font-bold uppercase ${fontK2dBold.className}`}
             >
               {lang.home.redesign.hero.title}
             </h1>
             <h2
-              className={`text-md lg:text-7xl pb-4 lg:pb-8 ${fontK2dBold.className}`}
+              className={`text-xl lg:text-7xl pb-4 lg:pb-8 ${fontK2dBold.className}`}
             >
               {lang.home.redesign.hero.subTitle}
             </h2>
             <p
-              className={`text-md lg:text-3xl pb-4 lg:pb-8 ${fontKantumruy_Pro.className}`}
+              className={`text-lg lg:text-3xl pb-4 lg:pb-8 ${fontKantumruy_Pro.className}`}
             >
               {lang.home.redesign.hero.slogan}
             </p>
